aabd3171eca48d5ba05d0a76901c1031d15dc6e7
tnauar <72252576+tnauar@users.noreply.github.com>
2026-08-26 14:05:15 +0300

    Final version of the class
    Final version of the class with comments.

--- python/FileWriter.py
+++ python/FileWriter.py
@@ -1,34 +1,49 @@
 import pandas as pd
-import pyarrow
 
 class FileWriter():
+
     def __init__(self):
         pass
 
-    def write(self, data, filetype):
+    def write(self, data: list[dict], filetype: str) -> int:
+        """Writes the random data to a file.
+        
+        Args:
+            data: List of dicts that contain the data.
+            filetype: A string that contains the type of the file. Values are csv, json, xlsx and parquet.
+            
+        Returns: 0 if everything went well. 1 if there was an exception.
+        """
 
         self.df = pd.DataFrame(data)
 
-        if filetype == "csv":
-            self.write_csv()
-        elif filetype == "json":
-            self.write_json()
-        elif filetype == "xlsx":
-            self.write_xlsx()
-        elif filetype == "parquet":
-            self.write_parquet()
-        else:
-            self.write_csv()
-            
+        try:
 
-    def write_csv(self):
-        self.df.to_csv("esimerkki.csv", index=False)
+            if filetype == "csv":
+                self.write_csv()
+            elif filetype == "json":
+                self.write_json()
+            elif filetype == "xlsx":
+                self.write_xlsx()
+            elif filetype == "parquet":
+                self.write_parquet()
+            else:
+                self.write_csv()
 
-    def write_json(self):
-        self.df.to_json("esimerkki.json", orient="records", force_ascii=False, indent=2)
+        except Exception as e:
+            print(f"Writing file failed because of {e}")
+            return 1
 
-    def write_xlsx(self):
-        self.df.to_excel("esimerkki.xlsx", index=False)
+        return 0
 
-    def write_parquet(self):
-        self.df.to_parquet("esimerkki.parquet", index=False)
+    def write_csv(self) -> None:
+        self.df.to_csv("./random_data/random_data_1.csv", index=False)
+
+    def write_json(self) -> None:
+        self.df.to_json("./random_data/random_data_2.json", orient="records", force_ascii=False, indent=2)
+
+    def write_xlsx(self) -> None:
+        self.df.to_excel("./random_data/random_data_3.xlsx", index=False)
+
+    def write_parquet(self) -> None:
+        self.df.to_parquet("./random_data/random_data_4.parquet", index=False)
